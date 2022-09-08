8aed4c68226b56c6d02ed18906aa176323a78606
richard246 <107892052+richard246@users.noreply.github.com>
2022-09-07 20:32:15 -0400

    User Location feature

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let input = document.getElementById("myText").value;
+let userButton = document.getElementById("userLocation");
 document.getElementById("myButton").onclick = function(){
     $('.temp').empty();
     $('.weather').empty();
@@ -51,5 +52,29 @@ $(document).on("keypress", "input", function(e){
         );
     }
 });
+userButton.onclick = function(){
+    $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=b2519172c48948ba9b73992f0886b06b",
+    function(data){
+        console.log(data.city);
+        $('.temp').empty();
+        $('.weather').empty();
+        var city = data.city;
+        
+        $.getJSON(
+            "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=052a9b7f2c0223088cf6f38f8b4f9fce",
+            function(data){
+        var icon = "https://openweathermap.org/img/w/"+data.weather[0].icon+".png"
+        var temp = data.main.temp;
+        var weather = data.weather[0].main
+    
+        $('.icon').attr('src', icon);
+        $('.temp').append(Math.floor(temp)+"°");
+        $('.weather').append(weather)
+       
+            }
+        );
+
+    })
+}
   
 
